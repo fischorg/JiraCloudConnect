597fe189c18d32f27dec373438b719ccd94e181b
Jeremiah Fischer <fischgeek@gmail.com>
2024-09-12 13:45:18 -0500

    quick

---
+++ CustomFields.fs
@@ -0,0 +1,6 @@
+﻿namespace JiraCloudConnect
+
+open APICalls
+
+module CustomFields =
+    let GetAllCustomFields () = client.Fields.GetCustomFieldsAsync() |> call
---
+++ Webhooks.fs
@@ -0,0 +1,9 @@
+﻿namespace JiraCloudConnect
+
+open FSharp.Core
+open FSharp.Data
+
+ module Webhooks =
+    type WebhookIssueEvent = JsonProvider<"C:\dev\JiraCloudConnect\Samples\WebhookIssueEvent.json", RootName="WebhookIssueEvent">
+
+    let ParseWebhook (data: string) = WebhookIssueEvent.Parse(data)
